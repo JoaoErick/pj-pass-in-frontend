c51119623b6d78a46ecd57bf8ccb3beb23826e55
JoaoErick <jerick1415@outlook.com>
2024-04-05 15:31:45 -0300

    feat: add register route

---
+++ src/app/register.tsx
@@ -0,0 +1,41 @@
+import { View, Image, StatusBar } from "react-native"
+import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
+import { Link } from "expo-router"
+
+import { Input } from "@/components/input"
+import { colors } from "@/styles/colors"
+import { Button } from "@/components/button"
+
+export default function Register() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content"></StatusBar>
+
+      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain"/>
+
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <FontAwesome6 
+            name="user-circle"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="Nome completo"></Input.Field>
+        </Input>
+
+        <Input>
+          <MaterialIcons 
+            name="alternate-email"
+            color={colors.green[200]}
+            size={20}
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address"></Input.Field>
+        </Input>
+
+        <Button title="Realizar inscrição"/>
+
+        <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">Já possui ingresso?</Link>
+      </View>
+    </View>
+  )
+}
