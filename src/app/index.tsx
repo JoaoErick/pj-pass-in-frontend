f4d921d82a8e0cb456f35c257b21c3b302f75315
JoaoErick <jerick1415@outlook.com>
2024-04-07 09:36:07 -0300

    feat: add data persistence in the device

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,41 +1,67 @@
 import { useState } from "react"
 import { View, Image, StatusBar, Alert } from "react-native"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
-import { Link } from "expo-router"
+import { Link, Redirect } from "expo-router"
 
 import { Input } from "@/components/input"
 import { colors } from "@/styles/colors"
 import { Button } from "@/components/button"
+import { api } from "@/server/api"
+import { useBadgeStore } from "@/store/badge-store"
 
 export default function Home() {
   const [code, setCode] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const badgeStore = useBadgeStore()
 
-  function handleAccessCredential() {
-    if (!code.trim()) {
-      return Alert.alert("Ingresso", "Informe o código do ingresso!")
+  async function handleAccessCredential() {
+    try {
+      if (!code.trim()) {
+        return Alert.alert("Ingresso", "Informe o código do ingresso!")
+      }
+
+      setIsLoading(true)
+
+      const { data } = await api.get(`/attendees/${code}/badge`)
+
+      badgeStore.save(data.badge)
+    } catch (error) {
+      console.log(error)
+
+      setIsLoading(false)
+
+      Alert.alert("Ingresso", "Ingresso não encontrado!")
     }
+  }
+
+  if (badgeStore.data?.checkInURL) {
+    return <Redirect href="/ticket" />
   }
 
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content"></StatusBar>
 
-      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain"/>
+      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
 
       <View className="w-full mt-12 gap-3">
         <Input>
-          <MaterialCommunityIcons 
+          <MaterialCommunityIcons
             name="ticket-confirmation-outline"
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field 
-            placeholder="Código do ingresso" 
-            onChangeText={ (value) => setCode(value) }
+          <Input.Field
+            placeholder="Código do ingresso"
+            onChangeText={(value) => setCode(value)}
           ></Input.Field>
         </Input>
 
-        <Button title="Acessar credencial" onPress={handleAccessCredential}/>
+        <Button
+          title="Acessar credencial"
+          onPress={handleAccessCredential}
+          isLoading={isLoading}
+        />
 
         <Link href="/register" className="text-gray-100 text-base font-bold text-center mt-8">Ainda não possui ingresso?</Link>
       </View>
