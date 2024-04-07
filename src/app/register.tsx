f08c0f7ba3f616ed152d40f3fbd939809d0af4b9
JoaoErick <jerick1415@outlook.com>
2024-04-07 09:35:24 -0300

    feat: add data persistence in the device

--- src/app/register.tsx
+++ src/app/register.tsx
@@ -3,48 +3,92 @@ import { View, Image, StatusBar, Alert } from "react-native"
 import { FontAwesome6, MaterialIcons } from "@expo/vector-icons"
 import { Link, router } from "expo-router"
 
-import { Input } from "@/components/input"
 import { colors } from "@/styles/colors"
+
+import { api } from "@/server/api"
+
+import { Input } from "@/components/input"
 import { Button } from "@/components/button"
+import axios from "axios"
+import { useBadgeStore } from "@/store/badge-store"
+
+const EVENT_ID = "2483a90a-a2bb-49b9-8595-2fa900fc6850"
 
 export default function Register() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
 
-  function handleRegister() {
-    if (!name.trim() || !email.trim()) {
-      return Alert.alert("Inscrição", "Preencha todos os campos!")
+  const badgeStore = useBadgeStore()
+
+  async function handleRegister() {
+    try {
+
+      if (!name.trim() || !email.trim()) {
+        return Alert.alert("Inscrição", "Preencha todos os campos!")
+      }
+
+      setIsLoading(true)
+
+      const registerResponse = await api.post(`/events/${EVENT_ID}/attendees`, {
+        name,
+        email
+      })
+
+      if (registerResponse.data.attendeeId) {
+        const badgeResponse = await api.get(`/attendees/${registerResponse.data.attendeeId}/badge`)
+
+        badgeStore.save(badgeResponse.data.badge)
+
+        Alert.alert("Inscrição", "Inscrição realizada com sucesso!", [
+          { text: "OK", onPress: () => router.push("/ticket") }
+        ])
+      }
+
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+
+      if (axios.isAxiosError(error)) {
+        if (String(error.response?.data.message).includes("already registered")) {
+          return Alert.alert("Inscrição", "Este e-mail já está cadastrado!")
+        }
+      }
+
+      Alert.alert("Inscrição", "Não foi possível fazer a inscrição")
     }
-
-    router.push("/ticket")
   }
 
   return (
     <View className="flex-1 bg-green-500 items-center justify-center p-8">
       <StatusBar barStyle="light-content"></StatusBar>
 
-      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain"/>
+      <Image source={require("@/assets/logo.png")} className="h-16" resizeMode="contain" />
 
       <View className="w-full mt-12 gap-3">
         <Input>
-          <FontAwesome6 
+          <FontAwesome6
             name="user-circle"
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="Nome completo" onChangeText={ (value) => setName(value) }></Input.Field>
+          <Input.Field placeholder="Nome completo" onChangeText={(value) => setName(value)}></Input.Field>
         </Input>
 
         <Input>
-          <MaterialIcons 
+          <MaterialIcons
             name="alternate-email"
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder="E-mail" keyboardType="email-address" onChangeText={ (value) => setEmail(value) }></Input.Field>
+          <Input.Field placeholder="E-mail" keyboardType="email-address" onChangeText={(value) => setEmail(value)}></Input.Field>
         </Input>
 
-        <Button title="Realizar inscrição" onPress={handleRegister}/>
+        <Button
+          title="Realizar inscrição"
+          onPress={handleRegister}
+          isLoading={isLoading}
+        />
 
         <Link href="/" className="text-gray-100 text-base font-bold text-center mt-8">Já possui ingresso?</Link>
       </View>
